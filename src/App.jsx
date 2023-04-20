3391c9405e3e55631d1ff90e3fc6dc2d88d08f0c
Lare1426 <laurikj.virta@gmail.com>
2023-04-20 18:38:05 +0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import styles from "./App.module.scss";
 
 function App() {
-  return <h1>Yes</h1>;
+  return (
+    <header className={styles.appHeader}>
+      <h1>Satisfactory Recipes</h1>
+      <hr></hr>
+    </header>
+  );
 }
 
 export default App;
