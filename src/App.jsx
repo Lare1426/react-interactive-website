5d9aca938bb16830be3ea4056182a3dca1b3cfee
Lare1426 <laurikj.virta@gmail.com>
2023-04-20 18:51:48 +0300

    add image and resolve alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import styles from "./App.module.scss";
+import satisfactoryPng from "@/assets/satisfactory.png";
 
 function App() {
   return (
-    <header className={styles.appHeader}>
-      <h1>Satisfactory Recipes</h1>
-      <hr></hr>
-    </header>
+    <>
+      <header className={styles.appHeader}>
+        <h1>Satisfactory Recipes</h1>
+        <hr></hr>
+      </header>
+      <main>
+        <div>
+          <img src={satisfactoryPng} />
+        </div>
+      </main>
+    </>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     modules: {
       localsConvention: "camelCase",
       generateScopedName: "[local]__[hash:base64:5]",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
